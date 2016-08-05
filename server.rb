b0dfc832f7a7b9666592b0fbe3672e50557eb712
Harry Jaggs <harryjaggs92@gmail.com>
2016-08-05 10:32:08 +0100

    server.rb update - incomplete

--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'json'
 
-set :public_folder, proc { File.join(root) }
+set :public_folder, proc { File.join(root) }git st
 
 get '/temperature' do
   headers 'Access-Control-Allow-Origin' => '*'
